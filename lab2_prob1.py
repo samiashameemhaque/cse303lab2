dc85dd765cec5416eb8bce76b95566661f537d09
samiashameemhaque <100278611+samiashameemhaque@users.noreply.github.com>
2022-03-10 02:31:38 +0600

    Add files via upload

---
+++ lab2_prob1.py
@@ -0,0 +1,3 @@
+num = [i for i in range(1, 1001) if i % 8 == 0]
+print('The numbers from 1–1000 that are divisible by 8 are given below: ')
+print(num)
---
+++ lab2_prob2.py
@@ -0,0 +1,3 @@
+num = [i for i in range(1, 1001) if '6' in str(i)]
+print("The numbers from 1–1000 that have a 6 in them are given below: ")
+print(num)
